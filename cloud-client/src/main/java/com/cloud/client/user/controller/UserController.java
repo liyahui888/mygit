dc6e4b848a887220368c7f7372afc7c18359486d
liyahui888 <648267806@qq.com>
2020-01-08 13:19:35 +0800

    配置中心实现

--- cloud-client/src/main/java/com/cloud/client/user/controller/UserController.java
+++ cloud-client/src/main/java/com/cloud/client/user/controller/UserController.java
@@ -3,19 +3,15 @@ package com.cloud.client.user.controller;
 import com.cloud.client.user.entity.User;
 import com.cloud.client.user.feign.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
+@RefreshScope //开启更新功能
 public class UserController {
     @Autowired
     private UserFeignClient userFeignClient;
@@ -23,5 +19,18 @@ public class UserController {
     public User findById(@PathVariable Long id) {
         User user = userFeignClient.findById(id);
         return user;
+    }
+
+
+    @Value("${server.port}")
+    private String fromValue;
+
+    /**
+     * 返回配置文件中的值
+     */
+    @GetMapping("/from")
+    @ResponseBody
+    public String returnFormValue(){
+        return fromValue;
     }
 }
