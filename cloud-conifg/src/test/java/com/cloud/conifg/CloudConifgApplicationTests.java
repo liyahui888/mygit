f3df9ce041630abb70d8ee708113c4a3899d728b
liyahui888 <648267806@qq.com>
2020-01-14 13:38:16 +0800

    多线程学习二

---
+++ cloud-conifg/src/test/java/com/cloud/conifg/CloudConifgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.conifg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConifgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
